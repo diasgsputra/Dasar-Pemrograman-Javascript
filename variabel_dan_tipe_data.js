c15e88fa58e580077430b4e03f6e01edd504a567
diasgsputra <60877754+diasgsputra@users.noreply.github.com>
2021-07-09 03:54:46 +0700

    update 9 juli

---
+++ variabel_dan_tipe_data.js
@@ -0,0 +1,22 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = "Dias";
+let lastName = "Galih";
+let age = 22;
+let isMarried = false;
+
+/**
+ * Hiraukan kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
